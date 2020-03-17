63108c466dc78ddc2a588b98c8ed5a357c307e7b
Dmytro <dimasoltusyuk@gmail.com>
2020-03-18 00:53:47 +0200

    init

---
+++ basic/views/layouts/main.php
@@ -0,0 +1,85 @@
+<?php
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+use app\widgets\Alert;
+use yii\helpers\Url;
+use yii\helpers\Html;
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+use yii\widgets\Breadcrumbs;
+use app\assets\AppAsset;
+
+if (class_exists('yii\debug\Module')) {
+    $this->off(\yii\web\View::EVENT_END_BODY, [\yii\debug\Module::getInstance(), 'renderToolbar']);
+}
+
+Yii::$app->name = 'Echo-Eko';
+
+$this->registerCssFile("@web/css/navbar.css");
+$this->registerCssFile("@web/css/animate.css");
+$this->registerCssFile("@web/css/all.min.css");
+$this->registerCssFile("https://fonts.googleapis.com/css?family=Inter:400,500,600,700,800,900&display=swap");
+
+AppAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php $this->registerCsrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
+<body>
+<?php $this->beginBody() ?>
+
+<div class="wrap">
+    <?php
+    NavBar::begin([
+        'brandLabel' => Yii::$app->name,
+        'brandUrl' => Yii::$app->homeUrl,
+        'options' => [
+            'class' => 'navbar-inverse navbar-fixed-top',
+        ],
+    ]);
+    ?>
+
+    <?php
+
+    echo Nav::widget([
+        'options' => ['class' => 'navbar-nav navbar-right'],
+        'items' => [
+            ['label' => 'Home', 'url' => ['/site/index']],
+            ['label' => 'About', 'url' => ['/site/about']],
+//            Html::a('Order', Url::toRoute('site/about', 'https'), ['class' => 'order-button']),
+            ['label' => 'Contact', 'url' => ['/site/contact']],
+            ['label' => 'Order', 'url' => ['/site/contact']],
+        ],
+    ]);
+    NavBar::end();
+    ?>
+
+    <div class="container">
+        <?= Breadcrumbs::widget([
+            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+        ]) ?>
+        <?= Alert::widget() ?>
+        <?= $content ?>
+    </div>
+</div>
+
+<footer class="footer">
+    <div class="container">
+        <p class="text-center">&copy; Echo-Eko <?= date('Y') ?></p>
+    </div>
+</footer>
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
---
+++ basic/views/site/index.php
@@ -0,0 +1,67 @@
+<?php
+
+use yii\helpers\Url;
+
+/* @var $this yii\web\View */
+
+$this->title = 'Echo-Eko';
+$this->registerCssFile('@web/css/index.css')
+
+?>
+
+<div class="site-index">
+    <div id="myCarousel" class="carousel slide" data-ride="carousel"">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+            <div class="item active">
+                <img src="https://images.wallpaperscraft.ru/image/glitchart_linii_pikseli_121670_1280x720.jpg" alt="">
+            </div>
+
+            <div class="item">
+                <img src="https://images.wallpaperscraft.ru/image/steklo_kapli_mokryj_130747_1280x720.jpg" alt="">
+            </div>
+
+            <div class="item">
+                <img src="https://images.wallpaperscraft.ru/image/neon_konstruktsiia_svechenie_123932_1280x720.jpg" alt="">
+            </div>
+        </div>
+
+        <!-- Left and right controls -->
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+    <hr>
+    <div class="cardsHolder">
+        <h1>We Render Services at Rent and Upkeep for Mobile Sanitary Systems</h1>
+        <div class="flex-center">
+            <div class="card">
+                <a href="<?= Url::toRoute('rent') ?>" class="fas fa-person-dolly card-icon"></a>
+                <a href="<?= Url::toRoute('rent') ?>">Rent</a>
+            </div>
+            <div class="card">
+                <a href="<?= Url::toRoute('service') ?>" class="fas fa-recycle card-icon"></a>
+                <a href="<?= Url::toRoute('service') ?>">Service</a>
+            </div>
+            <div class="card">
+                <a href="<?= Url::toRoute('store') ?>" class="fas fa-shopping-cart card-icon" style="padding-left: 30px;"></a>
+                <a href="<?= Url::toRoute('store') ?>">Sale</a>
+            </div>
+        </div>
+    </div>
+<!--    <div class="toiletsCounter">-->
+<!--        <h1>Toilets calc</h1>-->
+<!--    </div>-->
+</div>
