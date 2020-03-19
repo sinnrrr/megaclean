829e5cf7f9a9259da64ecff7485a802e972b4f95
Dmytro <dimasoltusyuk@gmail.com>
2020-03-19 20:40:54 +0200

    Update db.php

--- basic/config/db.php
+++ basic/config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=127.0.0.1;dbname=echo-eko',
     'username' => 'root',
-    'password' => '1532112351',
+    'password' => '',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
